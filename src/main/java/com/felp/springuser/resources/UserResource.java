633b0aaad88adfdf8c9b744d2e80f8e99181a50d
Felipe Cruz <felp.cruz.comp@gmail.com>
2025-10-07 03:01:37 -0300

    Service layer, component registration

--- src/main/java/com/felp/springuser/resources/UserResource.java
+++ src/main/java/com/felp/springuser/resources/UserResource.java
@@ -1,19 +1,36 @@
 package com.felp.springuser.resources;
 
 import com.felp.springuser.entities.User;
+import com.felp.springuser.repositories.UserRepository;
+import com.felp.springuser.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 //LINK: http://localhost:8080/users
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findall(){
-        User u = new User(1L, "Maria", "maria@gmail.com", "999999999", "1234");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findall(){
+        List<User> list = userService.findAll();
+
+        return ResponseEntity.ok().body(list);
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(user);
+    }
+
 }
---
+++ src/main/java/com/felp/springuser/services/UserService.java
@@ -0,0 +1,26 @@
+package com.felp.springuser.services;
+
+import com.felp.springuser.entities.User;
+import com.felp.springuser.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.orElse(null);
+    }
+}
